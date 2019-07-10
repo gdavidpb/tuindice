3702f03491f12180a96a63bc4a5c6ddf3c46500f
gdavidpb <gerardoprado93@gmail.com>
2019-07-10 12:49:59 -0400

    auth interceptor fix

--- app/src/main/java/com/gdavidpb/tuindice/data/source/service/DstAuthInterceptor.kt
+++ app/src/main/java/com/gdavidpb/tuindice/data/source/service/DstAuthInterceptor.kt
@@ -13,19 +13,24 @@ open class DstAuthInterceptor : Interceptor {
         val interceptedRequest = chain.proceed(firstRequest)
         val interceptedBody = interceptedRequest.body()
         val interceptedResponse = Jsoup.parse(interceptedBody?.string() ?: "")
-        val accessKey = interceptedResponse.selectFirst("input[name=lt]").attr("value")
+        val ltInput = interceptedResponse.selectFirst("input[name=lt]")
+        val accessKey = ltInput?.attr("value")
 
         interceptedBody?.close()
 
-        val firstBody = firstRequest.body().bodyToString()
-        val lastBody = "$firstBody&lt=$accessKey"
+        return if (accessKey != null) {
+            val firstBody = firstRequest.body().bodyToString()
+            val lastBody = "$firstBody&lt=$accessKey"
 
-        val lastRequest = firstRequest
-                .newBuilder()
-                .addHeader("Accept-Language", "es-VE")
-                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded;charset=UTF-8"), lastBody))
-                .build()
+            val lastRequest = firstRequest
+                    .newBuilder()
+                    .addHeader("Accept-Language", "es-VE")
+                    .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded;charset=UTF-8"), lastBody))
+                    .build()
 
-        return chain.proceed(lastRequest)
+            chain.proceed(lastRequest)
+        } else {
+            chain.proceed(firstRequest)
+        }
     }
 }
