448378cbfb0ed628e1266c50efd566abc18f42a4
gdavidpb <gerardoprado93@gmail.com>
2019-08-25 15:04:06 -0400

    Gradle update

--- app/build/generated/source/buildConfig/debug/com/gdavidpb/tuindice/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gdavidpb/tuindice/BuildConfig.java
@@ -20,4 +20,6 @@ public final class BuildConfig {
   public static final String ENDPOINT_DST_SECURE = "https://secure.dst.usb.ve/";
   public static final String URL_APP_BASE = "tuindice-usb.firebaseapp.com";
   public static final String URL_APP_PRIVACY_POLICY = "https://tuindice-usb.firebaseapp.com/";
+  public static final String URL_APP_RESET_PASSWORD = "https://tuindice-usb.firebaseapp.com?resetPassword=%s";
+  public static final String URL_APP_VERIFY = "https://tuindice-usb.firebaseapp.com?verify=%s";
 }
