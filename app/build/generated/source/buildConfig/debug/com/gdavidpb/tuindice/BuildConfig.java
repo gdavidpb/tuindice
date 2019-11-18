2d6091d5c393838297d0d56b036af7ae57f060a3
gdavidpb <gerardoprado93@gmail.com>
2019-11-18 08:54:12 -0300

    Optimizations for release

--- app/build/generated/source/buildConfig/debug/com/gdavidpb/tuindice/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gdavidpb/tuindice/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.gdavidpb.tuindice";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 7;
+  public static final int VERSION_CODE = 8;
   public static final String VERSION_NAME = "1.0";
   // Fields from build type: debug
   public static final String ENDPOINT_DST_ENROLLMENT = "http://comprobante.dii.usb.ve/nuevoComprobante/";
