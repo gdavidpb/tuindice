6aadd479db5243a10cfd0b7320d1a1cd903efb91
gdavidpb <gerardoprado93@gmail.com>
2020-06-22 00:30:24 -0400

    Gradle update

--- app/src/main/java/com/gdavidpb/tuindice/utils/extensions/ByteArray.kt
+++ app/src/main/java/com/gdavidpb/tuindice/utils/extensions/ByteArray.kt
@@ -2,4 +2,4 @@ package com.gdavidpb.tuindice.utils.extensions
 
 import android.util.Base64
 
-fun ByteArray.base64(): String = Base64.encodeToString(this, Base64.DEFAULT)
+fun ByteArray.base64(): String = Base64.encodeToString(this, Base64.NO_WRAP)
