a1a74c091732bcb4b9f55139abff446f4c850c31
gdavidpb <gerardoprado93@gmail.com>
2020-06-23 23:27:27 -0400

    Extra.kt improvements

--- app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Extra.kt
+++ app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Extra.kt
@@ -7,8 +7,16 @@ import okhttp3.RequestBody
 import okio.Buffer
 import org.jsoup.Jsoup
 import java.io.ByteArrayInputStream
+import java.util.zip.Deflater
 import java.util.zip.DeflaterInputStream
 import java.util.zip.InflaterInputStream
+
+private val sensitiveData = arrayOf(
+        "(?<=password=)[^&\\s\\n]+",    /* Password param  */
+        "(?<=JSESSIONID=)[^;]+",        /* JSession cookie */
+        "(?<=jsessionid=)[^\"?]+",      /* JSession href   */
+        "(?<=ticket=)[^&\\s\\n]+"       /* Ticket param    */
+).map { it.toRegex() }
 
 fun RequestBody?.bodyToString(): String {
     if (this == null) return ""
@@ -23,12 +31,15 @@ fun RequestBody?.bodyToString(): String {
 }
 
 fun String.deflateHtml(): String = let(Jsoup::parse)
-        .html()
+        .removeCommentsAndStyles()
+        .outerHtml()
+        .replace("(<[^>]*>)".toRegex()) { "\n${it.value}\n" }
+        .replace("[\\s\\n\\t]+$".toRegex(RegexOption.MULTILINE), "")
+        .replace("\n", "")
         .toByteArray()
         .deflate()
 
-fun ByteArray.deflate(): String = let(::ByteArrayInputStream)
-        .let(::DeflaterInputStream)
+fun ByteArray.deflate(): String = DeflaterInputStream(let(::ByteArrayInputStream), Deflater().apply { setLevel(Deflater.BEST_COMPRESSION) })
         .use { stream -> stream.readBytes() }
         .let { bytes -> Base64.encodeToString(bytes, Base64.DEFAULT) }
 
@@ -37,8 +48,9 @@ fun String.inflate(): ByteArray = Base64.decode(this, Base64.DEFAULT)
         .let(::InflaterInputStream)
         .use { stream -> stream.readBytes() }
 
-fun String.noSensitiveData(): String = replace("(username|password)=[^&]+&".toRegex(), "")
-        .run { if (isHtml()) deflateHtml() else this }
+fun String.noSensitiveData(): String = sensitiveData.fold(this) { acc, regex ->
+    acc.replace(regex, "******")
+}.run { if (isHtml()) deflateHtml() else this }
 
 fun String.isHtml() = contains("<[^>]*>".toRegex())
 
