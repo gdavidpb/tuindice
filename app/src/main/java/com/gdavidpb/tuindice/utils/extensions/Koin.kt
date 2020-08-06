f1359c45eae706e5ba0888fefe651c489183739f
gdavidpb <gerardoprado93@gmail.com>
2020-08-06 17:41:07 -0400

    Koin restart fix

--- app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Koin.kt
+++ app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Koin.kt
@@ -1,10 +1,17 @@
 package com.gdavidpb.tuindice.utils.extensions
 
+import com.gdavidpb.tuindice.BuildConfig
 import com.gdavidpb.tuindice.di.modules.appModule
+import com.gdavidpb.tuindice.modules.mockModule
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 
 fun restartKoinModules() {
-    unloadKoinModules(appModule)
-    loadKoinModules(appModule)
+    if (BuildConfig.DEBUG) {
+        unloadKoinModules(mockModule)
+        loadKoinModules(mockModule)
+    } else {
+        unloadKoinModules(appModule)
+        loadKoinModules(appModule)
+    }
 }
