f3e14ce36fa5cdd1378a2f84166525223d36a17b
gdavidpb <gerardoprado93@gmail.com>
2021-02-03 20:22:17 -0300

    Task await improved

--- app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Coroutine.kt
+++ app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Coroutine.kt
@@ -4,12 +4,6 @@ import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import kotlin.coroutines.suspendCoroutine
 
-@JvmName("awaitVoid")
-suspend fun Task<Void>.await() = suspendCoroutine<Unit> { continuation ->
-    runCatching { Tasks.await(this) }.map { Unit }.also(continuation::resumeWith)
-}
-
-@JvmName("awaitTResult")
 suspend fun <T> Task<T>.await() = suspendCoroutine<T> { continuation ->
-    runCatching { Tasks.await(this) }.also(continuation::resumeWith)
+    runCatching { Tasks.await(this) }.let(continuation::resumeWith)
 }
