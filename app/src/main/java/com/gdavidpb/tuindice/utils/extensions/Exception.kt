fc8dc755ffb2af691b3262c4944d67fc14d92445
gdavidpb <gerardoprado93@gmail.com>
2021-03-20 23:52:03 -0300

    Exception fix

--- app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Exception.kt
+++ app/src/main/java/com/gdavidpb/tuindice/utils/extensions/Exception.kt
@@ -6,6 +6,7 @@ import com.gdavidpb.tuindice.domain.model.exception.NoEnrolledException
 import com.google.firebase.auth.FirebaseAuthActionCodeException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.storage.StorageException
 import retrofit2.HttpException
 import java.io.InterruptedIOException
@@ -19,20 +20,29 @@ fun List<Throwable>.isInvalidLink() = contains<FirebaseAuthActionCodeException>(
 
 fun List<Throwable>.isAccountDisabled() = contains<FirebaseAuthInvalidUserException>()
 
-fun Throwable.isObjectNotFound() = this is StorageException &&
-        (errorCode == StorageException.ERROR_OBJECT_NOT_FOUND ||
-                errorCode == StorageException.ERROR_BUCKET_NOT_FOUND ||
-                errorCode == StorageException.ERROR_PROJECT_NOT_FOUND)
+fun Throwable.isObjectNotFound() = this is StorageException && when (errorCode) {
+    StorageException.ERROR_OBJECT_NOT_FOUND -> true
+    StorageException.ERROR_BUCKET_NOT_FOUND -> true
+    StorageException.ERROR_PROJECT_NOT_FOUND -> true
+    else -> false
+}
 
-fun Throwable?.isInvalidCredentials() =
-        this is FirebaseAuthInvalidCredentialsException ||
-                ((this as? AuthenticationException)?.code) == AuthResponseCode.INVALID_CREDENTIALS
+fun Throwable?.isInvalidCredentials() = when (this) {
+    is FirebaseAuthInvalidCredentialsException -> true
+    is AuthenticationException -> (code == AuthResponseCode.INVALID_CREDENTIALS)
+    else -> false
+}
 
-fun List<Throwable>.isUserNotFound() = find<FirebaseAuthInvalidUserException>()?.errorCode == "ERROR_USER_NOT_FOUND"
+fun Throwable?.isUserNotFound() = when (this) {
+    is FirebaseAuthInvalidUserException -> errorCode == "ERROR_USER_NOT_FOUND"
+    else -> false
+}
 
-fun Throwable.isNotEnrolled() =
-        this is NoEnrolledException ||
-                (this as? AuthenticationException)?.code == AuthResponseCode.NOT_ENROLLED
+fun Throwable.isNotEnrolled() = when (this) {
+    is NoEnrolledException -> true
+    is AuthenticationException -> (code == AuthResponseCode.NOT_ENROLLED)
+    else -> false
+}
 
 fun Throwable.isConnectionIssue() = when (this) {
     is SocketException -> true
@@ -42,14 +52,16 @@ fun Throwable.isConnectionIssue() = when (this) {
     is SSLHandshakeException -> true
     is HttpException -> true
     is ExecutionException -> true
+    is FirebaseFirestoreException -> (code == FirebaseFirestoreException.Code.UNAVAILABLE)
     else -> false
 }
 
-tailrec fun Throwable.causes(causes: HashSet<Throwable> = hashSetOf(this)): List<Throwable> {
-    val throwableCause = cause ?: return listOf(this)
+fun Throwable.causes(): List<Throwable> {
+    val hashSet = hashSetOf(this)
 
-    return if (causes.add(throwableCause))
-        throwableCause.causes(causes)
-    else
-        causes.toList()
+    var throwableCause = cause
+
+    while (throwableCause?.let(hashSet::add) == true) throwableCause = throwableCause.cause
+
+    return hashSet.toList()
 }
