f01cb01935b687b81e8d340d9a26349ae4baacf0
gdavidpb <gerardoprado93@gmail.com>
2021-04-11 01:46:15 -0400

    SignInViewModel unused code removed

--- app/src/main/java/com/gdavidpb/tuindice/presentation/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/gdavidpb/tuindice/presentation/viewmodel/SignInViewModel.kt
@@ -3,29 +3,18 @@ package com.gdavidpb.tuindice.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import com.gdavidpb.tuindice.domain.model.Credentials
 import com.gdavidpb.tuindice.domain.usecase.SignInUseCase
-import com.gdavidpb.tuindice.domain.usecase.SyncAccountUseCase
 import com.gdavidpb.tuindice.domain.usecase.errors.SignInError
-import com.gdavidpb.tuindice.domain.usecase.errors.SyncError
 import com.gdavidpb.tuindice.utils.extensions.LiveEvent
-import com.gdavidpb.tuindice.utils.extensions.LiveResult
 import com.gdavidpb.tuindice.utils.extensions.execute
 
 class SignInViewModel(
-        private val signInUseCase: SignInUseCase,
-        private val syncAccountUseCase: SyncAccountUseCase
+        private val signInUseCase: SignInUseCase
 ) : ViewModel() {
     val signIn = LiveEvent<Unit, SignInError>()
-    val sync = LiveResult<Boolean, SyncError>()
 
     fun signIn(usbId: String, password: String) {
-        val credentials = Credentials(
-                usbId = usbId,
-                password = password
-        )
+        val credentials = Credentials(usbId = usbId, password = password)
 
         execute(useCase = signInUseCase, params = credentials, liveData = signIn)
     }
-
-    fun syncAccount() =
-            execute(useCase = syncAccountUseCase, params = Unit, liveData = sync)
 }
