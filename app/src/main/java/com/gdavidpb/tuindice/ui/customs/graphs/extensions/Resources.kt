c77e3292dce1e316d7be8693388941b873836f43
gdavidpb <gerardoprado93@gmail.com>
2022-09-10 11:22:19 -0400

    AnimatedBackgroundView improvements

--- app/src/main/java/com/gdavidpb/tuindice/ui/customs/graphs/extensions/Resources.kt
+++ app/src/main/java/com/gdavidpb/tuindice/ui/customs/graphs/extensions/Resources.kt
@@ -12,21 +12,27 @@ import androidx.annotation.StyleableRes
 import androidx.core.content.ContextCompat
 
 fun TypedArray.getInterpolator(
-        context: Context,
-        @StyleableRes index: Int,
-        @AnimRes defValue: Int
+	context: Context,
+	@StyleableRes index: Int,
+	@AnimRes defValue: Int
 ): Interpolator = getResourceId(index, defValue)
-        .let { resId -> AnimationUtils.loadInterpolator(context, resId) }
+	.let { resId -> AnimationUtils.loadInterpolator(context, resId) }
 
 fun TypedArray.getDimension(
-        @StyleableRes index: Int,
-        @DimenRes defValue: Int
+	@StyleableRes index: Int,
+	@DimenRes defValue: Int
 ): Float = getResourceId(index, defValue)
-        .let { resId -> resources.getDimension(resId) }
+	.let { resId -> resources.getDimension(resId) }
 
 fun TypedArray.getDrawable(
-		context: Context,
-        @StyleableRes index: Int,
-        @DrawableRes defValue: Int
+	context: Context,
+	@StyleableRes index: Int,
+	@DrawableRes defValue: Int
 ): Drawable? = getResourceId(index, defValue)
-        .let { resId -> ContextCompat.getDrawable(context, resId) }
+	.let { resId -> ContextCompat.getDrawable(context, resId) }
+
+inline fun <reified T> TypedArray.getEnum(
+	@StyleableRes index: Int,
+	defValue: T
+): T = getInt(index, Int.MIN_VALUE)
+	.let { value -> T::class.java.enumConstants?.get(value) } ?: defValue
