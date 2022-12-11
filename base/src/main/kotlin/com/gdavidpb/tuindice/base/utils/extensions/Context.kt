01e00a95381b08dd69d46744e6e97ed4f8b9d17e
gdavidpb <gerardoprado93@gmail.com>
2022-12-11 17:02:43 -0300

    Remove unused imports from Context.kt

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/Context.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/Context.kt
@@ -2,13 +2,8 @@ package com.gdavidpb.tuindice.base.utils.extensions
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Rect
-import android.graphics.drawable.Drawable
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
-import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.preference.PreferenceManager
 import com.gdavidpb.tuindice.base.utils.ResourcesManager
 
