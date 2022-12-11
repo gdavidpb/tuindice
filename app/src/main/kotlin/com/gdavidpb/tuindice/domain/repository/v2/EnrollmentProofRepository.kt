9ce436a0141d4ea2fcc507385136919a565a4c7e
gdavidpb <gerardoprado93@gmail.com>
2022-12-11 16:24:39 -0300

    Remove temporal repositories

--- app/src/main/kotlin/com/gdavidpb/tuindice/domain/repository/v2/EnrollmentProofRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.gdavidpb.tuindice.domain.repository.v2
-
-import com.gdavidpb.tuindice.base.domain.model.EnrollmentProof
-
-interface EnrollmentProofRepository {
-	suspend fun getEnrollmentProof(): EnrollmentProof
-}
--- app/src/main/kotlin/com/gdavidpb/tuindice/domain/repository/v2/ProfilePictureRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.gdavidpb.tuindice.domain.repository.v2
-
-import java.io.InputStream
-
-interface ProfilePictureRepository {
-	suspend fun getProfilePicture(): String
-	suspend fun updateProfilePicture(stream: InputStream): String
-	suspend fun deleteProfilePicture()
-}
