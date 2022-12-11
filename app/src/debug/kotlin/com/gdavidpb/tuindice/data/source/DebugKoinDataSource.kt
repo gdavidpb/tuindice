8c1d4ff59bf98d2b27018ee51a45682b0770aa08
gdavidpb <gerardoprado93@gmail.com>
2022-12-11 17:53:30 -0300

    Fix dependencies injection repository implementation

--- app/src/debug/kotlin/com/gdavidpb/tuindice/data/source/DebugKoinDataSource.kt
+++ app/src/debug/kotlin/com/gdavidpb/tuindice/data/source/DebugKoinDataSource.kt
@@ -1,7 +1,12 @@
 package com.gdavidpb.tuindice.data.source
 
+import com.gdavidpb.tuindice.base.di.baseModule
 import com.gdavidpb.tuindice.base.domain.repository.DependenciesRepository
-import com.gdavidpb.tuindice.di.modules.mockModule
+import com.gdavidpb.tuindice.di.modules.appMockModule
+import com.gdavidpb.tuindice.evaluations.di.modules.evaluationsModule
+import com.gdavidpb.tuindice.login.di.modules.loginModule
+import com.gdavidpb.tuindice.record.di.modules.recordModule
+import com.gdavidpb.tuindice.summary.di.modules.summaryModule
 import org.koin.core.annotation.KoinReflectAPI
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
@@ -9,7 +14,25 @@ import org.koin.core.context.unloadKoinModules
 @KoinReflectAPI
 class DebugKoinDataSource : DependenciesRepository {
 	override fun restart() {
-		unloadKoinModules(mockModule)
-		loadKoinModules(mockModule)
+		unloadKoinModules(
+			listOf(
+				appMockModule,
+				baseModule,
+				loginModule,
+				summaryModule,
+				recordModule,
+				evaluationsModule
+			)
+		)
+		loadKoinModules(
+			listOf(
+				appMockModule,
+				baseModule,
+				loginModule,
+				summaryModule,
+				recordModule,
+				evaluationsModule
+			)
+		)
 	}
 }
--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/dependencies/ReleaseKoinDataSource.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/dependencies/ReleaseKoinDataSource.kt
@@ -1,15 +1,38 @@
 package com.gdavidpb.tuindice.data.source.dependencies
 
+import com.gdavidpb.tuindice.base.di.baseModule
 import com.gdavidpb.tuindice.di.modules.appModule
 import com.gdavidpb.tuindice.base.domain.repository.DependenciesRepository
+import com.gdavidpb.tuindice.evaluations.di.modules.evaluationsModule
+import com.gdavidpb.tuindice.login.di.modules.loginModule
+import com.gdavidpb.tuindice.record.di.modules.recordModule
+import com.gdavidpb.tuindice.summary.di.modules.summaryModule
 import org.koin.core.annotation.KoinReflectAPI
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 
 @KoinReflectAPI
 class ReleaseKoinDataSource : DependenciesRepository {
-    override fun restart() {
-        unloadKoinModules(appModule)
-        loadKoinModules(appModule)
-    }
+	override fun restart() {
+		unloadKoinModules(
+			listOf(
+				appModule,
+				baseModule,
+				loginModule,
+				summaryModule,
+				recordModule,
+				evaluationsModule
+			)
+		)
+		loadKoinModules(
+			listOf(
+				appModule,
+				baseModule,
+				loginModule,
+				summaryModule,
+				recordModule,
+				evaluationsModule
+			)
+		)
+	}
 }
