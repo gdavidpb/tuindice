1e9c050f775e734ef128702cde53c2654f80decb
gdavidpb <gerardoprado93@gmail.com>
2022-12-23 00:15:01 -0300

    Implement getSubject database operation

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/daos/SubjectDao.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/daos/SubjectDao.kt
@@ -1,7 +1,16 @@
 package com.gdavidpb.tuindice.data.source.room.daos
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.gdavidpb.tuindice.data.source.room.entities.SubjectEntity
+import com.gdavidpb.tuindice.data.source.room.schema.SubjectTable
 
 @Dao
-interface SubjectDao : BaseDao<SubjectEntity>
+interface SubjectDao : BaseDao<SubjectEntity> {
+	@Query(
+		"SELECT * FROM ${SubjectTable.TABLE_NAME} " +
+				"WHERE ${SubjectTable.ACCOUNT_ID} = :uid " +
+				"AND ${SubjectTable.ID} = :sid"
+	)
+	suspend fun getSubject(uid: String, sid: String): SubjectEntity
+}
