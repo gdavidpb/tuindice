2b9f4d66aa77fea6d760913059759156821abe6b
gdavidpb <gerardoprado93@gmail.com>
2022-12-23 00:53:07 -0300

    Implement getEvaluation database operation

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/daos/EvaluationDao.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/daos/EvaluationDao.kt
@@ -1,7 +1,16 @@
 package com.gdavidpb.tuindice.data.source.room.daos
 
 import androidx.room.Dao
+import androidx.room.Query
 import com.gdavidpb.tuindice.data.source.room.entities.EvaluationEntity
+import com.gdavidpb.tuindice.data.source.room.schema.EvaluationTable
 
 @Dao
-interface EvaluationDao : BaseDao<EvaluationEntity>
+interface EvaluationDao : BaseDao<EvaluationEntity> {
+	@Query(
+		"SELECT * FROM ${EvaluationTable.TABLE_NAME} " +
+				"WHERE ${EvaluationTable.ACCOUNT_ID} = :uid " +
+				"AND ${EvaluationTable.ID} = :eid"
+	)
+	suspend fun getEvaluation(uid: String, eid: String): EvaluationEntity
+}
