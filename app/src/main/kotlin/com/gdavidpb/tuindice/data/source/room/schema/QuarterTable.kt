99887be7ac87bbe362e1540230d3b018f40d9563
gdavidpb <gerardoprado93@gmail.com>
2022-12-22 23:01:41 -0300

    Rename columns in database schema

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/schema/QuarterTable.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/schema/QuarterTable.kt
@@ -3,12 +3,12 @@ package com.gdavidpb.tuindice.data.source.room.schema
 object QuarterTable {
 	const val TABLE_NAME = "quarters"
 
-	const val ID = "id"
-	const val ACCOUNT_ID = "a_id"
-	const val STATUS = "status"
-	const val GRADE = "grade"
-	const val GRADE_SUM = "grade_sum"
-	const val CREDITS = "credits"
-	const val START_DATE = "start_date"
-	const val END_DATE = "end_date"
+	const val ID = "quarter_id"
+	const val ACCOUNT_ID = "quarter_account_id"
+	const val STATUS = "quarter_status"
+	const val GRADE = "quarter_grade"
+	const val GRADE_SUM = "quarter_grade_sum"
+	const val CREDITS = "quarter_credits"
+	const val START_DATE = "quarter_start_date"
+	const val END_DATE = "quarter_end_date"
 }
--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/schema/SubjectTable.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/room/schema/SubjectTable.kt
@@ -3,12 +3,12 @@ package com.gdavidpb.tuindice.data.source.room.schema
 object SubjectTable {
 	const val TABLE_NAME = "subjects"
 
-	const val ID = "id"
-	const val QUARTER_ID = "q_id"
-	const val ACCOUNT_ID = "a_id"
-	const val CODE = "code"
-	const val NAME = "name"
-	const val CREDITS = "credits"
-	const val GRADE = "grade"
-	const val STATUS = "status"
+	const val ID = "subject_id"
+	const val QUARTER_ID = "subject_quarter_id"
+	const val ACCOUNT_ID = "subject_account_id"
+	const val CODE = "subject_code"
+	const val NAME = "subject_name"
+	const val CREDITS = "subject_credits"
+	const val GRADE = "subject_grade"
+	const val STATUS = "subject_status"
 }
