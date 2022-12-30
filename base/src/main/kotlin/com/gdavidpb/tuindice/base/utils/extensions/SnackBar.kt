8df03d8b1c06dd72ae89834b5f83a09a512f14ae
gdavidpb <gerardoprado93@gmail.com>
2022-12-30 17:13:25 -0300

    Remove unused import from SnackBar

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/SnackBar.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/SnackBar.kt
@@ -9,8 +9,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
-import com.gdavidpb.tuindice.base.utils.extensions.contentView
-import com.gdavidpb.tuindice.base.utils.extensions.getCompatColor
 import com.google.android.material.snackbar.BaseTransientBottomBar.*
 import com.google.android.material.snackbar.Snackbar
 
