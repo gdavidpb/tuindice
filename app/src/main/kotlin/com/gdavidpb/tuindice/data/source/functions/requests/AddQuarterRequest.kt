174db1abb020102b23c7e5cc57b6ba0e9b5df5dc
gdavidpb <gerardoprado93@gmail.com>
2023-01-20 02:07:44 -0300

    Remove unused files

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/functions/requests/AddQuarterRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.gdavidpb.tuindice.data.source.functions.requests
-
-import com.google.gson.annotations.SerializedName
-
-data class AddQuarterRequest(
-    @SerializedName("quarter") val quarter: Int,
-    @SerializedName("year") val year: Int,
-    @SerializedName("subjects") val subjects: List<AddSubjectRequest>
-)
--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/functions/requests/UpdateQuarterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.gdavidpb.tuindice.data.source.functions.requests
-
-import com.google.gson.annotations.SerializedName
-
-data class UpdateQuarterRequest(
-	@SerializedName("qid") val quarterId: String,
-	@SerializedName("subjects") val subjects: List<UpdateSubjectRequest>
-)
