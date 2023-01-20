251ceb65acfaa37f73154d1a3a8330febc6a3f5c
gdavidpb <gerardoprado93@gmail.com>
2023-01-20 01:07:14 -0300

    Add UpdatePasswordBottomSheetDialog to navigation

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/Navigation.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extensions/Navigation.kt
@@ -1,7 +1,15 @@
 package com.gdavidpb.tuindice.base.utils.extensions
 
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 
 fun NavController.popStackToRoot() {
-    while (previousBackStackEntry != null) popBackStack()
+	while (previousBackStackEntry != null) popBackStack()
+}
+
+fun DialogFragment.navigate(directions: NavDirections, navOptions: NavOptions? = null) {
+	requireParentFragment().findNavController().navigate(directions, navOptions)
 }
