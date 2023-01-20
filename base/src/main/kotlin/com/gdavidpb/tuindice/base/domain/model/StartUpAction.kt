2990ec28a291cfa72deac70f95c7a15e628ddb71
gdavidpb <gerardoprado93@gmail.com>
2023-01-19 22:33:15 -0300

    Remove getAccount from StartUpUseCase

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/model/StartUpAction.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/model/StartUpAction.kt
@@ -1,6 +1,6 @@
 package com.gdavidpb.tuindice.base.domain.model
 
 sealed class StartUpAction {
-	data class Main(val screen: Int, val account: Account) : StartUpAction()
+	data class Main(val screen: Int) : StartUpAction()
 	object SignIn : StartUpAction()
 }
