4ba505836946288d988cd788d9ff36dd48db1c69
gdavidpb <gerardoprado93@gmail.com>
2023-02-07 17:26:55 -0300

    Move some reportingRepository to BaseUseCase

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/usecase/baseV2/FlowUseCase.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/usecase/baseV2/FlowUseCase.kt
@@ -1,7 +1,5 @@
 package com.gdavidpb.tuindice.base.domain.usecase.baseV2
 
-import com.gdavidpb.tuindice.base.domain.repository.ReportingRepository
-import com.gdavidpb.tuindice.base.utils.ReportKeys
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
@@ -9,8 +7,6 @@ import kotlinx.coroutines.flow.*
 abstract class FlowUseCase<P, T, E>(
 	protected open val backgroundDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : BaseUseCase<P, Flow<T>, E>() {
-	protected abstract val reportingRepository: ReportingRepository
-
 	fun execute(params: P): Flow<UseCaseState<T, E>> {
 		return flow {
 			emitAll(executeOnBackground(params))
@@ -30,11 +26,5 @@ abstract class FlowUseCase<P, T, E>(
 
 				emit(UseCaseState.Error(error))
 			}
-	}
-
-	private fun logException(throwable: Throwable, error: E?) {
-		reportingRepository.setCustomKey(ReportKeys.USE_CASE, "${this::class.simpleName}")
-		reportingRepository.setCustomKey(ReportKeys.HANDLED, error != null)
-		reportingRepository.logException(throwable)
 	}
 }
