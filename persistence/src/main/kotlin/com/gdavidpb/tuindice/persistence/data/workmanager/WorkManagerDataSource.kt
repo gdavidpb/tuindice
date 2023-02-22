a453a38ac15e73b5d95202ee44b0bdc39863e351
gdavidpb <gerardoprado93@gmail.com>
2023-02-22 02:03:21 -0300

    Add backoff criteria to SyncWorker

--- persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/data/workmanager/WorkManagerDataSource.kt
+++ persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/data/workmanager/WorkManagerDataSource.kt
@@ -2,7 +2,9 @@ package com.gdavidpb.tuindice.persistence.data.workmanager
 
 import androidx.work.*
 import com.gdavidpb.tuindice.persistence.data.tracker.source.SchedulerDataSource
+import com.gdavidpb.tuindice.persistence.utils.WorkerBackoff
 import com.gdavidpb.tuindice.persistence.utils.WorkerName
+import java.util.concurrent.TimeUnit
 
 class WorkManagerDataSource(
 	private val workManager: WorkManager
@@ -14,6 +16,11 @@ class WorkManagerDataSource(
 
 		val syncWorker = OneTimeWorkRequestBuilder<SyncWorker>()
 			.setConstraints(constraints)
+			.setBackoffCriteria(
+				backoffPolicy = BackoffPolicy.EXPONENTIAL,
+				backoffDelay = WorkerBackoff.SYNC_WORKER,
+				timeUnit = TimeUnit.MILLISECONDS
+			)
 			.build()
 
 		workManager.enqueueUniqueWork(WorkerName.SYNC_WORKER, ExistingWorkPolicy.KEEP, syncWorker)
--- persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/utils/Constants.kt
+++ persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.gdavidpb.tuindice.persistence.utils
 
+import java.util.concurrent.TimeUnit
+
 object WorkerName {
 	const val SYNC_WORKER = "syncWorker"
+}
+
+object WorkerBackoff {
+	val SYNC_WORKER = TimeUnit.HOURS.toMillis(1)
 }
