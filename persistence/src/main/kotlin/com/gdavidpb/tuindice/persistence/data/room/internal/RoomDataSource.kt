d48fdde3e1a22ac37dc9f6a9c9cc27626dd14bfc
gdavidpb <gerardoprado93@gmail.com>
2023-03-09 01:29:54 -0300

    Add applyResolutions operation

--- persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/data/room/internal/RoomDataSource.kt
+++ persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/data/room/internal/RoomDataSource.kt
@@ -5,6 +5,7 @@ import com.gdavidpb.tuindice.persistence.data.room.TuIndiceDatabase
 import com.gdavidpb.tuindice.persistence.data.room.internal.mapper.toTransaction
 import com.gdavidpb.tuindice.persistence.data.room.internal.mapper.toTransactionEntity
 import com.gdavidpb.tuindice.persistence.data.tracker.source.LocalDataSource
+import com.gdavidpb.tuindice.persistence.domain.model.Resolution
 import com.gdavidpb.tuindice.persistence.domain.model.Transaction
 import com.gdavidpb.tuindice.persistence.domain.model.TransactionAction
 import com.gdavidpb.tuindice.persistence.domain.model.TransactionStatus
@@ -12,6 +13,10 @@ import com.gdavidpb.tuindice.persistence.domain.model.TransactionStatus
 internal class RoomDataSource(
 	private val room: TuIndiceDatabase
 ) : LocalDataSource {
+	override suspend fun applyResolutions(resolutions: List<Resolution>) {
+		TODO("Not yet implemented")
+	}
+
 	override suspend fun getPendingTransactions(): List<Transaction> {
 		return room.transactions.getTransactions(status = TransactionStatus.PENDING)
 			.map { transactionEntity -> transactionEntity.toTransaction() }
