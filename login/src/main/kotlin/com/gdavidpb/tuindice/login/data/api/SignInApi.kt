5a7ce9248726024c0196c8ccaa35431eff09f608
gdavidpb <gerardoprado93@gmail.com>
2023-04-05 23:21:43 -0400

    Implement PublicApi annotation

--- login/src/main/kotlin/com/gdavidpb/tuindice/login/data/api/SignInApi.kt
+++ login/src/main/kotlin/com/gdavidpb/tuindice/login/data/api/SignInApi.kt
@@ -1,12 +1,14 @@
 package com.gdavidpb.tuindice.login.data.api
 
 import com.gdavidpb.tuindice.login.data.api.response.SignInResponse
+import com.gdavidpb.tuindice.login.domain.annotation.PublicApi
 import retrofit2.Response
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface SignInApi {
+	@PublicApi
 	@POST("signIn")
 	suspend fun signIn(
 		@Header("Authorization") basicToken: String,
