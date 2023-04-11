b75257a39f2e42d033d934cae0c85fb67a744f55
gdavidpb <gerardoprado93@gmail.com>
2023-04-11 00:35:34 -0400

    Refactor coroutines extensions

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Coroutines.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Coroutines.kt
@@ -2,12 +2,16 @@ package com.gdavidpb.tuindice.base.utils.extension
 
 import com.google.android.play.core.tasks.Tasks
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.coroutines.coroutineContext
 import kotlin.coroutines.suspendCoroutine
 import com.google.android.gms.tasks.Task as GMSTask
 import com.google.android.play.core.tasks.Task as CoreTask
+
+private const val TIMEOUT_NO_AWAIT = 10000L
 
 suspend fun <T> GMSTask<T>.awaitOrNull(): T? = runCatching { await() }.getOrNull()
 
@@ -15,5 +19,20 @@ suspend fun <T> CoreTask<T>.await(): T? = suspendCoroutine { continuation ->
 	runCatching { Tasks.await(this) }.also(continuation::resumeWith)
 }
 
-suspend fun noAwait(block: suspend () -> Unit) =
-	CoroutineScope(coroutineContext).launch { block() }
+suspend fun suspendNoAwait(block: suspend () -> Unit) {
+	CoroutineScope(coroutineContext).launch {
+		runCatching {
+			block()
+		}
+	}
+}
+
+fun noAwait(block: suspend () -> Unit) {
+	CoroutineScope(Dispatchers.IO).launch {
+		runCatching {
+			withTimeoutOrNull(TIMEOUT_NO_AWAIT) {
+				block()
+			}
+		}
+	}
+}
