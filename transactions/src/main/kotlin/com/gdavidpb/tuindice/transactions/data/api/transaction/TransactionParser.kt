445cf0235c8d4c073a0bd7f40c45839f2f3c3480
gdavidpb <gerardoprado93@gmail.com>
2023-04-13 21:35:10 -0400

    Move transactions models

--- transactions/src/main/kotlin/com/gdavidpb/tuindice/transactions/data/api/transaction/TransactionParser.kt
+++ transactions/src/main/kotlin/com/gdavidpb/tuindice/transactions/data/api/transaction/TransactionParser.kt
@@ -1,4 +1,4 @@
-package com.gdavidpb.tuindice.transactions.data.retrofit
+package com.gdavidpb.tuindice.transactions.data.api.transaction
 
 import com.gdavidpb.tuindice.base.domain.model.transaction.Transaction
 import okhttp3.Request
@@ -14,4 +14,3 @@ class TransactionParser(
 		return parser.parse(request)
 	}
 }
-
