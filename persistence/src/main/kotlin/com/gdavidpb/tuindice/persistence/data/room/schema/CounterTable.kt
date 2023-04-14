a2619ddda57477cf748e9b01eec5cc12eeb7343f
gdavidpb <gerardoprado93@gmail.com>
2023-04-13 20:46:04 -0400

    Add CounterDao to Room

---
+++ persistence/src/main/kotlin/com/gdavidpb/tuindice/persistence/data/room/schema/CounterTable.kt
@@ -0,0 +1,8 @@
+package com.gdavidpb.tuindice.persistence.data.room.schema
+
+object CounterTable {
+	const val TABLE_NAME = "counters"
+
+	const val ID = "counter_id"
+	const val VALUE = "counter_value"
+}
