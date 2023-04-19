903db705e14bc14a5832da1cd75676ee84661dff
gdavidpb <gerardoprado93@gmail.com>
2023-04-19 01:01:48 -0400

    Fix StartUp models

--- login/src/main/kotlin/com/gdavidpb/tuindice/login/domain/usecase/error/StartUpError.kt
+++ login/src/main/kotlin/com/gdavidpb/tuindice/login/domain/usecase/error/StartUpError.kt
@@ -3,6 +3,5 @@ package com.gdavidpb.tuindice.login.domain.usecase.error
 import com.gdavidpb.tuindice.base.domain.model.ServicesStatus
 
 sealed class StartUpError {
-    class NoServices(val servicesStatus: ServicesStatus) : StartUpError()
-    class NoConnection(val isNetworkAvailable: Boolean) : StartUpError()
+	class NoServices(val status: ServicesStatus) : StartUpError()
 }
