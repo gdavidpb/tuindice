98d44b14e9382bff22ee5a15e101a901c92fb8c2
gdavidpb <gerardoprado93@gmail.com>
2023-04-19 15:45:01 -0400

    Move ViewModel extensions

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/processor/BaseProcessor.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/processor/BaseProcessor.kt
@@ -2,10 +2,10 @@ package com.gdavidpb.tuindice.base.presentation.processor
 
 import com.gdavidpb.tuindice.base.domain.usecase.base.UseCaseState
 
-abstract class BaseProcessor<T, E, V, ViewEvent>(
+abstract class BaseProcessor<T, E, ViewState, ViewEvent>(
 	open val eventChannel: (ViewEvent) -> Unit
 ) {
-	abstract suspend fun processDataState(state: UseCaseState.Data<T, E>): V
-	abstract suspend fun processLoadingState(state: UseCaseState.Loading<T, E>): V
-	abstract suspend fun processErrorState(state: UseCaseState.Error<T, E>): V
+	abstract suspend fun processDataState(state: UseCaseState.Data<T, E>): ViewState
+	abstract suspend fun processLoadingState(state: UseCaseState.Loading<T, E>): ViewState
+	abstract suspend fun processErrorState(state: UseCaseState.Error<T, E>): ViewState
 }
--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Flow.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Flow.kt
@@ -5,36 +5,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import com.gdavidpb.tuindice.base.domain.usecase.base.FlowUseCase
 import com.gdavidpb.tuindice.base.domain.usecase.base.UseCaseState
-import com.gdavidpb.tuindice.base.presentation.processor.BaseProcessor
-import com.gdavidpb.tuindice.base.presentation.viewmodel.BaseViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-
-suspend fun <P, T, E, ViewState, ViewEvent, U : FlowUseCase<P, T, E>> BaseViewModel<ViewState, *, ViewEvent>.stateInWith(
-	useCase: U,
-	params: P,
-	processor: BaseProcessor<T, E, ViewState, ViewEvent>
-) {
-	useCase.execute(params)
-		.mapNotNull { state ->
-			when (state) {
-				is UseCaseState.Loading -> processor.processLoadingState(state)
-				is UseCaseState.Data -> processor.processDataState(state)
-				is UseCaseState.Error -> processor.processErrorState(state)
-				else -> null
-			}
-		}
-		.stateIn(
-			scope = viewModelScope,
-			started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
-			initialValue = currentState
-		)
-		.collect { state ->
-			setState(state)
-		}
-}
 
 fun <T> emptyStateFlow() = MutableStateFlow<T?>(null)
 
