3ac7c9020e09dab06f324812add60dceb59b68e4
gdavidpb <gerardoprado93@gmail.com>
2023-04-19 01:01:48 -0400

    Fix StartUp models

--- login/src/main/kotlin/com/gdavidpb/tuindice/login/domain/usecase/exceptionhandler/StartUpExceptionHandler.kt
+++ login/src/main/kotlin/com/gdavidpb/tuindice/login/domain/usecase/exceptionhandler/StartUpExceptionHandler.kt
@@ -2,22 +2,18 @@ package com.gdavidpb.tuindice.login.domain.usecase.exceptionhandler
 
 import com.gdavidpb.tuindice.base.domain.exception.ServicesUnavailableException
 import com.gdavidpb.tuindice.base.domain.repository.ApplicationRepository
-import com.gdavidpb.tuindice.base.domain.repository.NetworkRepository
 import com.gdavidpb.tuindice.base.domain.repository.ReportingRepository
 import com.gdavidpb.tuindice.base.domain.usecase.base.ExceptionHandler
-import com.gdavidpb.tuindice.base.utils.extension.isConnection
 import com.gdavidpb.tuindice.base.utils.extension.noAwait
 import com.gdavidpb.tuindice.login.domain.usecase.error.StartUpError
 
 class StartUpExceptionHandler(
-	private val networkRepository: NetworkRepository,
 	private val applicationRepository: ApplicationRepository,
 	override val reportingRepository: ReportingRepository
 ) : ExceptionHandler<StartUpError>() {
 	override fun parseException(throwable: Throwable): StartUpError? {
-		return when {
-			throwable is ServicesUnavailableException -> StartUpError.NoServices(throwable.servicesStatus)
-			throwable.isConnection() -> StartUpError.NoConnection(networkRepository.isAvailable())
+		return when (throwable) {
+			is ServicesUnavailableException -> StartUpError.NoServices(throwable.servicesStatus)
 			else -> {
 				noAwait {
 					applicationRepository.clearData()
