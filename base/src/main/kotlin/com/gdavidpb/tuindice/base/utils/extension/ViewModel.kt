a70c3f3dcd24758b64d351d46064d25191618e15
gdavidpb <gerardoprado93@gmail.com>
2023-04-19 22:44:15 -0400

    Fix MVI base

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/ViewModel.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/ViewModel.kt
@@ -1,13 +1,10 @@
 package com.gdavidpb.tuindice.base.utils.extension
 
-import androidx.lifecycle.viewModelScope
 import com.gdavidpb.tuindice.base.domain.usecase.base.FlowUseCase
 import com.gdavidpb.tuindice.base.domain.usecase.base.UseCaseState
 import com.gdavidpb.tuindice.base.presentation.processor.BaseProcessor
 import com.gdavidpb.tuindice.base.presentation.viewmodel.BaseViewModel
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.stateIn
 
 suspend fun <P, T, E, ViewState, ViewEvent, U : FlowUseCase<P, T, E>> BaseViewModel<ViewState, *, ViewEvent>.execute(
 	useCase: U,
@@ -23,11 +20,6 @@ suspend fun <P, T, E, ViewState, ViewEvent, U : FlowUseCase<P, T, E>> BaseViewMo
 				else -> null
 			}
 		}
-		.stateIn(
-			scope = viewModelScope,
-			started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
-			initialValue = currentState
-		)
 		.collect { state ->
 			setState(state)
 		}
