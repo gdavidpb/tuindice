361343fde6734d5f2a612f0d795db495c293dac7
gdavidpb <gerardoprado93@gmail.com>
2023-04-20 19:55:31 -0400

    Add canOpenFile to ApplicationRepository

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/android/AndroidApplicationDataSource.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/android/AndroidApplicationDataSource.kt
@@ -3,14 +3,20 @@ package com.gdavidpb.tuindice.data.android
 import android.content.Context
 import android.content.SharedPreferences
 import com.gdavidpb.tuindice.base.domain.repository.ApplicationRepository
+import com.gdavidpb.tuindice.base.utils.extension.canOpenFile
 import com.gdavidpb.tuindice.persistence.data.room.TuIndiceDatabase
 import com.gdavidpb.tuindice.persistence.data.room.schema.DatabaseModel
+import java.io.File
 
 class AndroidApplicationDataSource(
 	private val context: Context,
 	private val room: TuIndiceDatabase,
 	private val sharedPreferences: SharedPreferences
 ) : ApplicationRepository {
+	override suspend fun canOpenFile(path: String): Boolean {
+		return context.canOpenFile(file = File(path))
+	}
+
 	override suspend fun clearData() {
 		room.clearAllTables()
 		room.close()
--- base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/repository/ApplicationRepository.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/repository/ApplicationRepository.kt
@@ -1,5 +1,6 @@
 package com.gdavidpb.tuindice.base.domain.repository
 
 interface ApplicationRepository {
+	suspend fun canOpenFile(path: String): Boolean
 	suspend fun clearData()
 }
