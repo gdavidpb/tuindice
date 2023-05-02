01758ae7d978460f6cc49121c22d4afb93f76fe3
gdavidpb <gerardoprado93@gmail.com>
2023-05-02 19:51:43 -0400

    Reorder Base.kt

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/reducer/Base.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/reducer/Base.kt
@@ -4,21 +4,23 @@ import com.gdavidpb.tuindice.base.domain.usecase.base.UseCaseState
 import com.gdavidpb.tuindice.base.presentation.viewmodel.BaseViewModel
 import kotlinx.coroutines.flow.Flow
 
-interface ViewAction : ViewInput
-interface ViewState : ViewOutput
-interface ViewEvent : ViewOutput
-
 interface ViewInput
 interface ViewOutput
+
+interface ViewState : ViewOutput
+interface ViewAction : ViewInput
+interface ViewEvent : ViewOutput
 
 suspend inline fun <reified State : ViewState, reified Event : ViewEvent, T, E> Flow<UseCaseState<T, E>>.collect(
 	viewModel: BaseViewModel<State, *, Event>,
 	reducer: BaseReducer<State, Event, T, E>
-) = reducer
-	.reduce(this, viewModel::getCurrentState)
-	.collect { output ->
-		when (output) {
-			is State -> viewModel.setState(output)
-			is Event -> viewModel.sendEvent(output)
+) {
+	reducer
+		.reduce(useCaseFlow = this, stateProvider = viewModel::getCurrentState)
+		.collect { output ->
+			when (output) {
+				is State -> viewModel.setState(output)
+				is Event -> viewModel.sendEvent(output)
+			}
 		}
-	}
+}
