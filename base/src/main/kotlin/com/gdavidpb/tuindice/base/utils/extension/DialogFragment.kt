acd1c4d438b59a7a0b8bda5f051dbfdcf26ba98b
gdavidpb <gerardoprado93@gmail.com>
2023-05-03 22:10:24 -0400

    Remove popStackToRoot from extensions

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/DialogFragment.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/DialogFragment.kt
@@ -1,15 +1,9 @@
 package com.gdavidpb.tuindice.base.utils.extension
 
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
-
-// TODO check
-fun NavController.popStackToRoot() {
-	while (previousBackStackEntry != null) popBackStack()
-}
 
 fun DialogFragment.navigate(directions: NavDirections, navOptions: NavOptions? = null) {
 	requireParentFragment().findNavController().navigate(directions, navOptions)
