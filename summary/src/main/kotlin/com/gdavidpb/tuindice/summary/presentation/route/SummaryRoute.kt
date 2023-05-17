cdc940f147cbbeacbadd56b3ad3ae00bddd3bc5d
gdavidpb <gerardoprado93@gmail.com>
2023-05-17 01:43:43 -0400

    Add navigation file to Summary

---
+++ summary/src/main/kotlin/com/gdavidpb/tuindice/summary/presentation/route/SummaryRoute.kt
@@ -0,0 +1,9 @@
+package com.gdavidpb.tuindice.summary.presentation.route
+
+import androidx.compose.runtime.Composable
+import com.gdavidpb.tuindice.summary.ui.screen.SummaryScreen
+
+@Composable
+fun SummaryRoute() {
+	SummaryScreen()
+}
