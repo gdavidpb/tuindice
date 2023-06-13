96c19a492456e813591e6644ef021cf9927dd90d
gdavidpb <gerardoprado93@gmail.com>
2023-06-12 23:40:34 -0400

    Fix SignOutUseCase

--- app/src/main/kotlin/com/gdavidpb/tuindice/domain/usecase/SignOutUseCase.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/domain/usecase/SignOutUseCase.kt
@@ -1,6 +1,10 @@
 package com.gdavidpb.tuindice.domain.usecase
 
-import com.gdavidpb.tuindice.base.domain.repository.*
+import com.gdavidpb.tuindice.base.domain.repository.ApplicationRepository
+import com.gdavidpb.tuindice.base.domain.repository.AuthRepository
+import com.gdavidpb.tuindice.base.domain.repository.DependenciesRepository
+import com.gdavidpb.tuindice.base.domain.repository.MessagingRepository
+import com.gdavidpb.tuindice.base.domain.repository.SettingsRepository
 import com.gdavidpb.tuindice.base.domain.usecase.base.FlowUseCase
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
@@ -9,11 +13,13 @@ class SignOutUseCase(
 	private val authRepository: AuthRepository,
 	private val messagingRepository: MessagingRepository,
 	private val dependenciesRepository: DependenciesRepository,
-	private val applicationRepository: ApplicationRepository
+	private val applicationRepository: ApplicationRepository,
+	private val settingsRepository: SettingsRepository
 ) : FlowUseCase<Unit, Unit, Nothing>() {
 	override suspend fun executeOnBackground(params: Unit): Flow<Unit> {
 		messagingRepository.unsubscribeFromAllTopics()
 		authRepository.signOut()
+		settingsRepository.clear()
 		applicationRepository.clearData()
 		dependenciesRepository.restart()
 
