62a14365a98a0523c19f6af86dda0e503aa46140
gdavidpb <gerardoprado93@gmail.com>
2023-06-19 00:57:25 -0400

    Refactor date utils

--- summary/src/main/kotlin/com/gdavidpb/tuindice/summary/presentation/mapper/Account.kt
+++ summary/src/main/kotlin/com/gdavidpb/tuindice/summary/presentation/mapper/Account.kt
@@ -2,11 +2,9 @@ package com.gdavidpb.tuindice.summary.presentation.mapper
 
 import com.gdavidpb.tuindice.base.domain.model.Account
 import com.gdavidpb.tuindice.base.utils.extension.capitalize
+import com.gdavidpb.tuindice.base.utils.extension.daysDistance
 import com.gdavidpb.tuindice.base.utils.extension.format
-import com.gdavidpb.tuindice.base.utils.extension.isToday
-import com.gdavidpb.tuindice.base.utils.extension.isYesterday
 import java.util.Date
-import java.util.concurrent.TimeUnit
 
 fun Account.toShortName(): String {
 	val firstName = firstNames.substringBefore(' ')
@@ -16,15 +14,13 @@ fun Account.toShortName(): String {
 }
 
 fun Date.formatLastUpdate(): String {
-	val now = Date()
-	val diff = now.time - time
-	val days = TimeUnit.MILLISECONDS.toDays(diff)
+	val daysDistance = daysDistance()
 
 	return when {
 		time == 0L -> "Nunca"
-		isToday() -> format("'Hoy,' hh:mm aa")
-		isYesterday() -> format("'Ayer,' hh:mm aa")
-		days < 7 -> format("EEEE',' hh:mm aa")
+		daysDistance == 0 -> format("'Hoy,' hh:mm aa")
+		daysDistance == -1 -> format("'Ayer,' hh:mm aa")
+		daysDistance < 7 -> format("EEEE',' hh:mm aa")
 		else -> format("dd 'de' MMMM yyyy")
 	}?.capitalize() ?: "-"
 }
