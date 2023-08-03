38d30def824aad1e9893c82cb158026e206b0034
gdavidpb <gerardoprado93@gmail.com>
2023-08-03 15:41:09 -0400

    Add date to evaluation model

--- evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/ui/view/EvaluationContentView.kt
+++ evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/ui/view/EvaluationContentView.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.gdavidpb.tuindice.evaluations.R
 import com.gdavidpb.tuindice.evaluations.presentation.contract.Evaluation
+import java.util.Date
 
 @Composable
 fun EvaluationContentView(
