09cd2acc18dc17e1bb842eb5153ae1f8cc47ffdd
gdavidpb <gerardoprado93@gmail.com>
2023-08-03 18:37:34 -0400

    Fix GetEvaluationsUseCase

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Date.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Date.kt
@@ -3,19 +3,15 @@ package com.gdavidpb.tuindice.base.utils.extension
 import java.util.Calendar
 import java.util.Date
 import java.util.concurrent.TimeUnit
-import kotlin.math.round
+import kotlin.math.ceil
+
+private val millisecondsPerDay = TimeUnit.DAYS.toMillis(1)
+private val millisecondsPerWeek = TimeUnit.DAYS.toMillis(7)
 
 fun Date.daysDistance(): Int {
-	val now = Calendar.getInstance()
+	val distance = (time - System.currentTimeMillis()).toDouble()
 
-	val target = Calendar.getInstance().apply {
-		time = this@daysDistance
-	}
-
-	val millisecondsPerDay = TimeUnit.DAYS.toMillis(1)
-	val distance = (target.timeInMillis - now.timeInMillis).toDouble()
-
-	return round(distance / millisecondsPerDay).toInt()
+	return ceil(distance / millisecondsPerDay).toInt()
 }
 
 fun Date.weeksDistance(): Int {
@@ -26,8 +22,7 @@ fun Date.weeksDistance(): Int {
 		set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
 	}
 
-	val millisecondsPerWeek = TimeUnit.DAYS.toMillis(7)
 	val distance = (target.timeInMillis - now.timeInMillis).toDouble()
 
-	return round(distance / millisecondsPerWeek).toInt()
+	return ceil(distance / millisecondsPerWeek).toInt()
 }
