a4577c0cd07f4bca8a8bea62ebaea4ea121aba16
gdavidpb <gerardoprado93@gmail.com>
2023-08-22 01:46:48 -0400

    Fix gradle updates

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/ui/view/TopAppBarActionsView.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/ui/view/TopAppBarActionsView.kt
@@ -1,10 +1,9 @@
 package com.gdavidpb.tuindice.base.ui.view
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.with
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,7 +11,6 @@ import androidx.compose.runtime.Composable
 import com.gdavidpb.tuindice.base.presentation.model.TopBarAction
 import com.gdavidpb.tuindice.base.presentation.model.TopBarConfig
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun TopAppBarActionsView(
 	topBarConfig: TopBarConfig?,
@@ -24,8 +22,9 @@ fun TopAppBarActionsView(
 			val enter = slideInHorizontally { x -> x }
 			val exit = slideOutHorizontally { x -> -2 * x }
 
-			enter with exit
+			enter togetherWith exit
 		},
+		label = "TopAppBarActionsViewAnimatedContent",
 	) { targetState ->
 		if (targetState != null)
 			Row {
