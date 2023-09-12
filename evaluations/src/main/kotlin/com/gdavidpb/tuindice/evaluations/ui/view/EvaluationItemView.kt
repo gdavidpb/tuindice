734112ae1e926646531c2e58ec1de166ac1e26f6
gdavidpb <gerardoprado93@gmail.com>
2023-09-12 01:49:49 -0300

    Fix EvaluationItemView

--- evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/ui/view/EvaluationItemView.kt
+++ evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/ui/view/EvaluationItemView.kt
@@ -2,6 +2,7 @@ package com.gdavidpb.tuindice.evaluations.ui.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -23,8 +24,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
+import androidx.compose.ui.text.style.TextOverflow
 import com.gdavidpb.tuindice.evaluations.R
 
 @Composable
@@ -52,40 +52,15 @@ fun EvaluationItemView(
 				vertical = dimensionResource(id = R.dimen.dp_8)
 			)
 	) {
-		ConstraintLayout(
+		Box(
 			modifier = Modifier
 				.fillMaxWidth()
 				.padding(dimensionResource(id = R.dimen.dp_16))
 		) {
-			val (
-				textName,
-				textSubjectCode,
-				textDate,
-				textGrade,
-				dotNotification
-			) = createRefs()
-
-			Text(
-				modifier = Modifier
-					.constrainAs(textName) {
-						start.linkTo(parent.start)
-						top.linkTo(parent.top)
-
-						width = Dimension.fillToConstraints
-					},
-				text = name,
-				color = MaterialTheme.colorScheme.onBackground,
-				style = MaterialTheme.typography.titleLarge,
-				fontWeight = FontWeight.Bold
-			)
-
 			if (isNotGraded)
 				Box(
 					modifier = Modifier
-						.constrainAs(dotNotification) {
-							end.linkTo(parent.end)
-							top.linkTo(parent.top)
-						}
+						.align(Alignment.TopEnd)
 						.size(dimensionResource(id = R.dimen.dp_8))
 						.background(
 							color = MaterialTheme.colorScheme.error,
@@ -93,91 +68,58 @@ fun EvaluationItemView(
 						)
 				)
 
-			Row(
-				modifier = Modifier
-					.constrainAs(textSubjectCode) {
-						start.linkTo(textName.start)
-						top.linkTo(textName.bottom)
-
-						width = Dimension.fillToConstraints
-					},
-				verticalAlignment = Alignment.CenterVertically
-			) {
-				Icon(
-					modifier = Modifier
-						.size(dimensionResource(id = R.dimen.dp_16)),
-					imageVector = icon,
-					tint = MaterialTheme.colorScheme.outline,
-					contentDescription = null
-				)
-
+			Column {
 				Text(
 					modifier = Modifier
-						.padding(start = dimensionResource(id = R.dimen.dp_12)),
-					text = stringResource(id = R.string.evaluation_title, type, subjectCode),
-					style = MaterialTheme.typography.bodyMedium
-				)
-			}
-
-			Row(
-				modifier = Modifier
-					.constrainAs(textDate) {
-						start.linkTo(textSubjectCode.start)
-						top.linkTo(textSubjectCode.bottom)
-
-						width = Dimension.fillToConstraints
-					}
-					.padding(
-						top = dimensionResource(
-							id = if (isDone) R.dimen.dp_16 else R.dimen.dp_6
-						)
-					),
-				verticalAlignment = Alignment.CenterVertically
-			) {
-				Icon(
-					modifier = Modifier
-						.size(dimensionResource(id = R.dimen.dp_16)),
-					imageVector = if (isCompleted)
-						Icons.Outlined.Done
-					else
-						Icons.Outlined.CalendarToday,
-					tint = when {
-						isCompleted -> MaterialTheme.colorScheme.primary
-						isNotGraded -> MaterialTheme.colorScheme.error
-						else -> MaterialTheme.colorScheme.outline
-					},
-					contentDescription = null
+						.fillMaxWidth(),
+					text = name,
+					maxLines = 1,
+					overflow = TextOverflow.Ellipsis,
+					color = MaterialTheme.colorScheme.onBackground,
+					style = MaterialTheme.typography.titleLarge,
+					fontWeight = FontWeight.Bold
 				)
 
-				Text(
-					modifier = Modifier
-						.padding(start = dimensionResource(id = R.dimen.dp_12)),
-					text = date,
-					color = if (!isNotGraded)
-						Color.Unspecified
-					else
-						MaterialTheme.colorScheme.error,
-					style = MaterialTheme.typography.bodyMedium,
-					fontWeight = FontWeight.Light
-				)
-			}
-
-			if (isDone)
 				Row(
 					modifier = Modifier
-						.constrainAs(textGrade) {
-							start.linkTo(textDate.start)
-							top.linkTo(textDate.bottom)
-
-							width = Dimension.fillToConstraints
-						}
-						.padding(vertical = dimensionResource(id = R.dimen.dp_4)),
+						.fillMaxWidth(),
 					verticalAlignment = Alignment.CenterVertically
 				) {
 					Icon(
 						modifier = Modifier
 							.size(dimensionResource(id = R.dimen.dp_16)),
-						imageVector = Icons.Outlined.BookmarkBorder,
+						imageVector = icon,
+						tint = MaterialTheme.colorScheme.outline,
+						contentDescription = null
+					)
+
+					Text(
+						modifier = Modifier
+							.padding(start = dimensionResource(id = R.dimen.dp_12)),
+						text = stringResource(id = R.string.evaluation_title, type, subjectCode),
+						maxLines = 1,
+						overflow = TextOverflow.Ellipsis,
+						style = MaterialTheme.typography.bodyMedium
+					)
+				}
+
+				Row(
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(
+							top = dimensionResource(
+								id = if (isDone) R.dimen.dp_16 else R.dimen.dp_6
+							)
+						),
+					verticalAlignment = Alignment.CenterVertically
+				) {
+					Icon(
+						modifier = Modifier
+							.size(dimensionResource(id = R.dimen.dp_16)),
+						imageVector = if (isCompleted)
+							Icons.Outlined.Done
+						else
+							Icons.Outlined.CalendarToday,
 						tint = when {
 							isCompleted -> MaterialTheme.colorScheme.primary
 							isNotGraded -> MaterialTheme.colorScheme.error
@@ -189,17 +131,54 @@ fun EvaluationItemView(
 					Text(
 						modifier = Modifier
 							.padding(start = dimensionResource(id = R.dimen.dp_12)),
-						text = if (!isNotGraded)
-							stringResource(id = R.string.evaluation_grade, grade, maxGrade)
-						else
-							stringResource(id = R.string.evaluation_not_completed),
+						text = date,
+						maxLines = 1,
+						overflow = TextOverflow.Ellipsis,
 						color = if (!isNotGraded)
 							Color.Unspecified
 						else
 							MaterialTheme.colorScheme.error,
-						style = MaterialTheme.typography.bodyMedium
+						style = MaterialTheme.typography.bodyMedium,
+						fontWeight = FontWeight.Light
 					)
 				}
+
+				if (isDone)
+					Row(
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(vertical = dimensionResource(id = R.dimen.dp_4)),
+						verticalAlignment = Alignment.CenterVertically
+					) {
+						Icon(
+							modifier = Modifier
+								.size(dimensionResource(id = R.dimen.dp_16)),
+							imageVector = Icons.Outlined.BookmarkBorder,
+							tint = when {
+								isCompleted -> MaterialTheme.colorScheme.primary
+								isNotGraded -> MaterialTheme.colorScheme.error
+								else -> MaterialTheme.colorScheme.outline
+							},
+							contentDescription = null
+						)
+
+						Text(
+							modifier = Modifier
+								.padding(start = dimensionResource(id = R.dimen.dp_12)),
+							text = if (!isNotGraded)
+								stringResource(id = R.string.evaluation_grade, grade, maxGrade)
+							else
+								stringResource(id = R.string.evaluation_not_completed),
+							maxLines = 1,
+							overflow = TextOverflow.Ellipsis,
+							color = if (!isNotGraded)
+								Color.Unspecified
+							else
+								MaterialTheme.colorScheme.error,
+							style = MaterialTheme.typography.bodyMedium
+						)
+					}
+			}
 		}
 	}
 }
