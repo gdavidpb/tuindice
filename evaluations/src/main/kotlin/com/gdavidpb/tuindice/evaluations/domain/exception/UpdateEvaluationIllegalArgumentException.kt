24cd107f60ef66d8400953b0b18579f47d10bbfa
gdavidpb <gerardoprado93@gmail.com>
2023-11-17 02:47:02 -0300

    Fix UpdateEvaluationError

--- evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/domain/exception/UpdateEvaluationIllegalArgumentException.kt
+++
@@ -1,7 +0,0 @@
-package com.gdavidpb.tuindice.evaluations.domain.exception
-
-import com.gdavidpb.tuindice.evaluations.domain.usecase.error.UpdateEvaluationError
-
-class UpdateEvaluationIllegalArgumentException(
-	val error: UpdateEvaluationError
-) : IllegalArgumentException()
--- evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/domain/usecase/UpdateEvaluationUseCase.kt
+++ evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/domain/usecase/UpdateEvaluationUseCase.kt
@@ -4,19 +4,14 @@ import com.gdavidpb.tuindice.base.domain.repository.AuthRepository
 import com.gdavidpb.tuindice.base.domain.usecase.base.FlowUseCase
 import com.gdavidpb.tuindice.evaluations.domain.mapper.toEvaluationUpdate
 import com.gdavidpb.tuindice.evaluations.domain.repository.EvaluationRepository
-import com.gdavidpb.tuindice.evaluations.domain.usecase.error.UpdateEvaluationError
-import com.gdavidpb.tuindice.evaluations.domain.usecase.exceptionhandler.UpdateEvaluationExceptionHandler
 import com.gdavidpb.tuindice.evaluations.domain.usecase.param.UpdateEvaluationParams
-import com.gdavidpb.tuindice.evaluations.domain.usecase.validator.UpdateEvaluationParamsValidator
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 class UpdateEvaluationUseCase(
 	private val authRepository: AuthRepository,
-	private val evaluationRepository: EvaluationRepository,
-	override val paramsValidator: UpdateEvaluationParamsValidator,
-	override val exceptionHandler: UpdateEvaluationExceptionHandler
-) : FlowUseCase<UpdateEvaluationParams, Unit, UpdateEvaluationError>() {
+	private val evaluationRepository: EvaluationRepository
+) : FlowUseCase<UpdateEvaluationParams, Unit, Nothing>() {
 	override suspend fun executeOnBackground(params: UpdateEvaluationParams): Flow<Unit> {
 		val activeUId = authRepository.getActiveAuth().uid
 
