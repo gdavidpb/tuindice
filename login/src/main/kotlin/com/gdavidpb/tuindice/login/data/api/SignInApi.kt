c621b261acdcf414742b67a1b5b01d508f7dfbdb
gdavidpb <gerardoprado93@gmail.com>
2023-11-27 23:10:46 -0300

    Implement sign in attestation

--- login/src/main/kotlin/com/gdavidpb/tuindice/login/data/api/SignInApi.kt
+++ login/src/main/kotlin/com/gdavidpb/tuindice/login/data/api/SignInApi.kt
@@ -1,6 +1,8 @@
 package com.gdavidpb.tuindice.login.data.api
 
+import com.gdavidpb.tuindice.base.domain.annotation.AttestedApi
 import com.gdavidpb.tuindice.base.domain.annotation.PublicApi
+import com.gdavidpb.tuindice.login.data.api.attestation.SignInAttestation
 import com.gdavidpb.tuindice.login.data.api.response.SignInResponse
 import retrofit2.Response
 import retrofit2.http.Header
@@ -8,6 +10,7 @@ import retrofit2.http.POST
 
 interface SignInApi {
 	@PublicApi
+	@AttestedApi(SignInAttestation::class)
 	@POST("signIn")
 	suspend fun signIn(
 		@Header("Authorization") basicToken: String,
