36829523ac1e57d042268f15909bcd07e78aa218
gdavidpb <gerardoprado93@gmail.com>
2023-11-28 01:25:37 -0300

    Fix optionals request and response models

--- evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/data/repository/evaluation/source/api/request/AddEvaluationRequest.kt
+++ evaluations/src/main/kotlin/com/gdavidpb/tuindice/evaluations/data/repository/evaluation/source/api/request/AddEvaluationRequest.kt
@@ -1,4 +1,4 @@
-package com.gdavidpb.tuindice.evaluations.data.api.request
+package com.gdavidpb.tuindice.evaluations.data.repository.evaluation.source.api.request
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -8,7 +8,7 @@ data class AddEvaluationRequest(
 	@SerialName("reference") val reference: String,
 	@SerialName("subject_id") val subjectId: String,
 	@SerialName("type") val type: Int,
-	@SerialName("grade") val grade: Double?,
+	@SerialName("grade") val grade: Double? = null,
 	@SerialName("max_grade") val maxGrade: Double,
 	@SerialName("date") val date: Long,
 )
