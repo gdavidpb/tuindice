fe1ca4d809cb853ca31ff2ba218818c0e6565e86
gdavidpb <gerardoprado93@gmail.com>
2023-11-28 00:59:26 -0300

    Refactor login module packages

--- login/src/main/kotlin/com/gdavidpb/tuindice/login/data/api/mapper/SignIn.kt
+++
@@ -1,8 +0,0 @@
-package com.gdavidpb.tuindice.login.data.api.mapper
-
-import com.gdavidpb.tuindice.login.domain.model.SignIn
-import com.gdavidpb.tuindice.login.data.api.response.SignInResponse
-
-fun SignInResponse.toSignIn() = SignIn(
-	token = token
-)
