c7b6307f13f61b81ea8f834130086d79f814c96f
gdavidpb <gerardoprado93@gmail.com>
2023-11-29 20:20:42 -0300

    Remove EnqueuedApi annotation from all apis

--- record/src/main/kotlin/com/gdavidpb/tuindice/record/data/repository/quarter/RecordApi.kt
+++ record/src/main/kotlin/com/gdavidpb/tuindice/record/data/repository/quarter/RecordApi.kt
@@ -1,11 +1,8 @@
 package com.gdavidpb.tuindice.record.data.repository.quarter
 
-import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.parser.QuarterRemoveParser
-import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.parser.SubjectUpdateParser
 import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.request.UpdateSubjectRequest
 import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.response.QuarterResponse
 import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.response.SubjectResponse
-import com.gdavidpb.tuindice.transactions.domain.annotation.EnqueuedApi
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -13,13 +10,11 @@ interface RecordApi {
 	@GET("quarters")
 	suspend fun getQuarters(): Response<List<QuarterResponse>>
 
-	@EnqueuedApi(QuarterRemoveParser::class)
 	@DELETE("quarters")
 	suspend fun deleteQuarter(
 		@Query("qid") quarterId: String
 	): Response<Unit>
 
-	@EnqueuedApi(SubjectUpdateParser::class)
 	@PATCH("subjects")
 	suspend fun updateSubject(
 		@Body request: UpdateSubjectRequest
