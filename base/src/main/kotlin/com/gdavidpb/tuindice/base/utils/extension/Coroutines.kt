3c68923e675838cc42be6af8d7107f6730a5de94
gdavidpb <gerardoprado93@gmail.com>
2024-03-11 22:35:29 -0300

    Update gradle

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Coroutines.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Coroutines.kt
@@ -1,18 +1,11 @@
 package com.gdavidpb.tuindice.base.utils.extension
 
-import com.google.android.play.core.tasks.Task
-import com.google.android.play.core.tasks.Tasks
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
-import kotlin.coroutines.suspendCoroutine
 
 private const val TIMEOUT_NO_AWAIT = 10000L
-
-suspend fun <T> Task<T>.await(): T? = suspendCoroutine { continuation ->
-	runCatching { Tasks.await(this) }.also(continuation::resumeWith)
-}
 
 fun noAwait(block: suspend () -> Unit) {
 	CoroutineScope(Dispatchers.IO).launch {
