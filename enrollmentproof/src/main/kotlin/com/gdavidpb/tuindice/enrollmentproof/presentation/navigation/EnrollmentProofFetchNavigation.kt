1c4d72da6a108711e99f437b61990b6e797289d1
Gerardo Prado <gerardoprado93@gmail.com>
2024-05-27 11:14:11 -0400

    Update navigation compose lib

--- enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/presentation/navigation/EnrollmentProofFetchNavigation.kt
+++ enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/presentation/navigation/EnrollmentProofFetchNavigation.kt
@@ -1,18 +1,16 @@
 package com.gdavidpb.tuindice.enrollmentproof.presentation.navigation
 
+import androidx.compose.material.navigation.bottomSheet
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import com.gdavidpb.tuindice.base.presentation.model.SnackBarMessage
 import com.gdavidpb.tuindice.base.presentation.navigation.Destination
 import com.gdavidpb.tuindice.enrollmentproof.presentation.route.EnrollmentProofFetchRoute
-import com.google.accompanist.navigation.material.ExperimentalMaterialNavigationApi
-import com.google.accompanist.navigation.material.bottomSheet
 
 fun NavController.navigateToEnrollmentProofFetch() {
 	navigate(Destination.EnrollmentProofFetch.route)
 }
 
-@OptIn(ExperimentalMaterialNavigationApi::class)
 fun NavGraphBuilder.enrollmentProofFetchDialog(
 	navigateToUpdatePassword: () -> Unit,
 	onDismissRequest: () -> Unit,
