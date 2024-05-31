f7f410d58e353a1c81f1fde07d7258ef8f5ee6f2
Gerardo Prado <gerardoprado93@gmail.com>
2024-05-30 23:48:10 -0400

    Migrate recordApi to ktor

--- record/src/main/kotlin/com/gdavidpb/tuindice/record/data/repository/quarter/source/RecordApiDataSource.kt
+++ record/src/main/kotlin/com/gdavidpb/tuindice/record/data/repository/quarter/source/RecordApiDataSource.kt
@@ -1,37 +1,48 @@
 package com.gdavidpb.tuindice.record.data.repository.quarter.source
 
-import com.gdavidpb.tuindice.base.utils.extension.getOrThrow
-import com.gdavidpb.tuindice.record.data.repository.quarter.RecordApi
 import com.gdavidpb.tuindice.record.data.repository.quarter.RemoteDataSource
 import com.gdavidpb.tuindice.record.data.repository.quarter.model.RemoteQuarter
 import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.mapper.toAddQuarterRequest
 import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.mapper.toRemoteQuarter
+import com.gdavidpb.tuindice.record.data.repository.quarter.source.api.response.QuarterResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 
 class RecordApiDataSource(
-	private val recordApi: RecordApi
+	private val ktorClient: HttpClient
 ) : RemoteDataSource {
 	override suspend fun getQuarters(): List<RemoteQuarter> {
-		return recordApi.getQuarters()
-			.getOrThrow()
+		return ktorClient.get("quarters")
+			.body<List<QuarterResponse>>()
 			.map { quarterResponse -> quarterResponse.toRemoteQuarter() }
 	}
 
 	override suspend fun getQuarter(qid: String): RemoteQuarter {
-		return recordApi.getQuarter()
-			.getOrThrow()
+		return ktorClient.get("quarters") {
+			parameter("qid", qid)
+		}
+			.body<QuarterResponse>()
 			.toRemoteQuarter()
 	}
 
 	override suspend fun removeQuarter(qid: String) {
-		recordApi.deleteQuarter(qid)
-			.getOrThrow()
+		ktorClient.delete("quarters") {
+			parameter("qid", qid)
+		}
 	}
 
 	override suspend fun addQuarters(quarters: List<RemoteQuarter>): List<RemoteQuarter> {
 		val request = quarters.map { remoteQuarter -> remoteQuarter.toAddQuarterRequest() }
 
-		return recordApi.addQuarters(request)
-			.getOrThrow()
+		return ktorClient.post("quarters") {
+			setBody(request)
+		}
+			.body<List<QuarterResponse>>()
 			.map { quarterResponse -> quarterResponse.toRemoteQuarter() }
 	}
 }
