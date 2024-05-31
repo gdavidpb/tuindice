ba811eb304ee6128746c2bdf8a8c80f02324c41f
Gerardo Prado <gerardoprado93@gmail.com>
2024-05-30 23:59:32 -0400

    Migrate enrollmentProofApi to ktor

--- enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/data/repository/enrollmentproof/source/EnrollmentProofApiDataSource.kt
+++ enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/data/repository/enrollmentproof/source/EnrollmentProofApiDataSource.kt
@@ -1,17 +1,19 @@
 package com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source
 
-import com.gdavidpb.tuindice.base.utils.extension.getOrThrow
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.RemoteDataSource
-import com.gdavidpb.tuindice.enrollmentproof.domain.model.EnrollmentProof
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.api.mapper.toEnrollmentProof
-import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.EnrollmentProofApi
+import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.api.response.EnrollmentProofResponse
+import com.gdavidpb.tuindice.enrollmentproof.domain.model.EnrollmentProof
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 class EnrollmentProofApiDataSource(
-	private val enrollmentProofApi: EnrollmentProofApi
+	private val ktorClient: HttpClient
 ) : RemoteDataSource {
 	override suspend fun getEnrollmentProof(): EnrollmentProof {
-		return enrollmentProofApi.enrollmentProof()
-			.getOrThrow()
+		return ktorClient.get("enrollment-proof")
+			.body<EnrollmentProofResponse>()
 			.toEnrollmentProof()
 	}
 }
--- enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/di/EnrollmentProofModule.kt
+++ enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/di/EnrollmentProofModule.kt
@@ -1,15 +1,12 @@
 package com.gdavidpb.tuindice.enrollmentproof.di
 
-import com.gdavidpb.tuindice.base.BuildConfig
-import com.gdavidpb.tuindice.base.utils.extension.create
-import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.EnrollmentProofApiDataSource
-import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.EnrollmentProofApi
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.EnrollmentProofDataRepository
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.LocalDataSource
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.RemoteDataSource
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.StorageDataSource
-import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.RoomDataSource
+import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.EnrollmentProofApiDataSource
 import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.InternalStorageDataSource
+import com.gdavidpb.tuindice.enrollmentproof.data.repository.enrollmentproof.source.RoomDataSource
 import com.gdavidpb.tuindice.enrollmentproof.domain.repository.EnrollmentProofRepository
 import com.gdavidpb.tuindice.enrollmentproof.domain.usecase.FetchEnrollmentProofUseCase
 import com.gdavidpb.tuindice.enrollmentproof.domain.usecase.exceptionhandler.FetchEnrollmentProofExceptionHandler
@@ -19,7 +16,6 @@ import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
-import retrofit2.Retrofit
 
 val enrollmentProofModule = module {
 	/* View Models */
@@ -43,17 +39,6 @@ val enrollmentProofModule = module {
 	factoryOf(::RoomDataSource) { bind<LocalDataSource>() }
 	factoryOf(::EnrollmentProofApiDataSource) { bind<RemoteDataSource>() }
 	factoryOf(::InternalStorageDataSource) { bind<StorageDataSource>() }
-
-	/* Enrollment Proof Api */
-
-	single {
-		Retrofit.Builder()
-			.baseUrl(BuildConfig.ENDPOINT_TU_INDICE_API)
-			.addConverterFactory(get())
-			.client(get())
-			.build()
-			.create<EnrollmentProofApi>()
-	}
 
 	/* Exception handlers */
 
