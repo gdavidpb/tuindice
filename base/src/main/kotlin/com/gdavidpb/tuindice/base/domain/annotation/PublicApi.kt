fc7d0477e1bd202145f13daa202920141a26f4cb
Gerardo Prado <gerardoprado93@gmail.com>
2024-05-31 00:07:14 -0400

    Remove Retrofit utils

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/domain/annotation/PublicApi.kt
+++
@@ -1,4 +0,0 @@
-package com.gdavidpb.tuindice.base.domain.annotation
-
-@Target(AnnotationTarget.FUNCTION)
-annotation class PublicApi
--- base/src/main/kotlin/com/gdavidpb/tuindice/base/utils/extension/Annotation.kt
+++
@@ -1,6 +0,0 @@
-package com.gdavidpb.tuindice.base.utils.extension
-
-import com.gdavidpb.tuindice.base.domain.annotation.PublicApi
-import okhttp3.Request
-
-fun Request.isPublicApi() = hasAnnotation<PublicApi>()
