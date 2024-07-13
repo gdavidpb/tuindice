27115d80d48ce96cdb8bdd1e14617bcb79dcf16e
Gerardo Prado <gerardoprado93@gmail.com>
2024-07-13 13:44:06 -0400

    Improve BaseViewModel

--- base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/viewmodel/BaseViewModel.kt
+++ base/src/main/kotlin/com/gdavidpb/tuindice/base/presentation/viewmodel/BaseViewModel.kt
@@ -6,14 +6,15 @@ import com.gdavidpb.tuindice.base.presentation.Mutation
 import com.gdavidpb.tuindice.base.presentation.ViewAction
 import com.gdavidpb.tuindice.base.presentation.ViewEffect
 import com.gdavidpb.tuindice.base.presentation.ViewState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -23,17 +24,14 @@ abstract class BaseViewModel<S : ViewState, A : ViewAction, E : ViewEffect>(
 	initialState: S,
 	initialAction: A? = null
 ) : ViewModel() {
-	private val actionsChannel = Channel<A>()
-	private val effectsChannel = Channel<E>()
-	val effect = effectsChannel
-		.receiveAsFlow()
-
-	val state = actionsChannel
-		.receiveAsFlow()
+	val action = MutableSharedFlow<A>()
+	val effect = MutableSharedFlow<E>()
+	val state = action
 		.onStart { if (initialAction != null) emit(initialAction) }
 		.flatMapMerge { action -> processAction(action, ::sendEffect) }
 		.scan(initialState) { currentState, mutation -> mutation(currentState) }
 		.distinctUntilChanged()
+		.flowOn(Dispatchers.IO)
 		.stateIn(
 			scope = viewModelScope,
 			started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000L),
@@ -45,15 +43,15 @@ abstract class BaseViewModel<S : ViewState, A : ViewAction, E : ViewEffect>(
 		sideEffect: (E) -> Unit
 	): Flow<Mutation<S>>
 
-	protected fun sendAction(action: A) {
+	protected fun sendAction(viewAction: A) {
 		viewModelScope.launch {
-			actionsChannel.send(action)
+			action.emit(viewAction)
 		}
 	}
 
-	private fun sendEffect(effect: E) {
+	private fun sendEffect(viewEffect: E) {
 		viewModelScope.launch {
-			effectsChannel.send(effect)
+			effect.emit(viewEffect)
 		}
 	}
 }
