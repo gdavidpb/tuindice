2457ae9fecb5c5a016745bbdc22750dbf1023d74
Gerardo Prado <gerardoprado93@gmail.com>
2024-07-20 00:02:04 -0400

    Migrate enrollmentProof navigation

--- enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/presentation/navigation/EnrollmentProofFetchNavigation.kt
+++ enrollmentproof/src/main/kotlin/com/gdavidpb/tuindice/enrollmentproof/presentation/navigation/EnrollmentProofFetchNavigation.kt
@@ -1,22 +1,16 @@
 package com.gdavidpb.tuindice.enrollmentproof.presentation.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.dialog
 import com.gdavidpb.tuindice.base.presentation.model.SnackBarMessage
-import com.gdavidpb.tuindice.base.presentation.navigation.Destination
 import com.gdavidpb.tuindice.enrollmentproof.presentation.route.EnrollmentProofFetchRoute
 
-fun NavController.navigateToEnrollmentProofFetch() {
-	navigate(Destination.EnrollmentProofFetch)
-}
-
-fun NavGraphBuilder.enrollmentProofFetchDialog(
+fun NavGraphBuilder.enrollmentProofFetchNavigation(
 	navigateToUpdatePassword: () -> Unit,
 	onDismissRequest: () -> Unit,
 	showSnackBar: (message: SnackBarMessage) -> Unit
 ) {
-	dialog<Destination.EnrollmentProofFetch> {
+	dialog<EnrollmentProofFetchDestination> {
 		EnrollmentProofFetchRoute(
 			onNavigateToUpdatePassword = navigateToUpdatePassword,
 			onDismissRequest = onDismissRequest,
