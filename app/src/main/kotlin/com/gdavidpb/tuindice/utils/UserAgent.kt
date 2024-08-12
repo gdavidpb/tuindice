24f1c94f974556a08936a124c5c95b2868e93e0b
Gerardo Prado <gerardoprado93@gmail.com>
2024-08-12 19:54:51 -0400

    Refactor UserAgent

--- app/src/main/kotlin/com/gdavidpb/tuindice/utils/UserAgent.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/utils/UserAgent.kt
@@ -8,18 +8,6 @@ import com.gdavidpb.tuindice.base.utils.extension.versionName
 class UserAgent(
 	private val userAgent: String
 ) {
-	private val values = userAgent.split(";")
-
-	val appName = values[0]
-	val versionName = values[1]
-	val versionCode = values[2]
-	val osName = values[3]
-	val osVersion = values[4]
-	val osCode = values[5]
-	val osId = values[6]
-	val manufacturer = values[7]
-	val model = values[8]
-
 	constructor(context: Context) : this(
 		listOf(
 			"TuIndice",
