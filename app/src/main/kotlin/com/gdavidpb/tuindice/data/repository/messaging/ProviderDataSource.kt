56bddcf29b8beffc64803102420e7dfb50fa4635
Gerardo Prado <gerardoprado93@gmail.com>
2024-09-05 16:37:33 -0400

    Fix FirebaseMessagingDataSource

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/repository/messaging/ProviderDataSource.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/repository/messaging/ProviderDataSource.kt
@@ -2,6 +2,4 @@ package com.gdavidpb.tuindice.data.repository.messaging
 
 interface ProviderDataSource {
 	suspend fun getToken(): String?
-	suspend fun subscribeToTopic(topic: String)
-	suspend fun unsubscribeFromTopic(topic: String)
 }
--- app/src/main/kotlin/com/gdavidpb/tuindice/data/repository/messaging/source/FirebaseMessagingDataSource.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/repository/messaging/source/FirebaseMessagingDataSource.kt
@@ -10,12 +10,4 @@ class FirebaseMessagingDataSource(
 	override suspend fun getToken(): String? {
 		return runCatching { firebaseMessaging.token.await() }.getOrNull()
 	}
-
-	override suspend fun subscribeToTopic(topic: String) {
-		runCatching { firebaseMessaging.subscribeToTopic(topic).await() }.getOrNull()
-	}
-
-	override suspend fun unsubscribeFromTopic(topic: String) {
-		runCatching { firebaseMessaging.unsubscribeFromTopic(topic).await() }.getOrNull()
-	}
 }
