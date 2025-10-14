a8226803529de71778e0cfc39fc4a951bc90c63f
Gerardo Prado <gerardoprado93@gmail.com>
2025-10-13 23:45:44 -0300

    Update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,10 @@ android {
 
 			signingConfig = signingConfigs.getByName("release")
 
-			proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
 		}
 	}
 
