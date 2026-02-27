f4a7c5c593436eab0c69a423f5dfcafd5e36d885
Gerardo Prado <gerardoprado93@gmail.com>
2026-02-27 00:25:32 -0300

    Fix AndroidBrowserDataSource

--- app/src/main/kotlin/com/gdavidpb/tuindice/data/source/browser/AndroidBrowserDataSource.kt
+++ app/src/main/kotlin/com/gdavidpb/tuindice/data/source/browser/AndroidBrowserDataSource.kt
@@ -9,10 +9,10 @@ class AndroidBrowserDataSource(
 	private val context: Context
 ) : BrowserRepository {
 	override fun open(url: String) {
-		runCatching {
-			context.startActivity(
-				Intent(Intent.ACTION_VIEW, url.toUri())
-			)
-		}
+		context.startActivity(
+			Intent(Intent.ACTION_VIEW, url.toUri()).apply {
+				addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+			}
+		)
 	}
 }
