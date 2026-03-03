dea44078f919424cae0beaa999cbd76bd13d3a48
Gerardo Prado <gerardoprado93@gmail.com>
2026-03-03 02:43:45 -0300

    Fix SettingsStore

--- base/src/commonMain/kotlin/com/gdavidpb/tuindice/base/data/source/settings/SettingsStores.kt
+++ base/src/commonMain/kotlin/com/gdavidpb/tuindice/base/data/source/settings/SettingsStores.kt
@@ -1,4 +1,4 @@
 package com.gdavidpb.tuindice.base.data.source.settings
 
 const val APP_STORE_NAME = "tuindice"
-const val APP_SECURE_STORE_NAME = "tuindice_secure"
+const val APP_SECURE_STORE_NAME = "tuindicesecure"
