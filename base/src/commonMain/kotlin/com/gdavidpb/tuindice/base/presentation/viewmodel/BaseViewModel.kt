53d2147f8a1f2785c6a0e1c44bc77c93716b2580
Gerardo Prado <gerardoprado93@gmail.com>
2026-06-11 15:21:40 -0400

    Refactor `BaseViewModel`: simplify state and effect handling, remove unused methods, and consolidate event publishing logic.

--- base/src/commonMain/kotlin/com/gdavidpb/tuindice/base/presentation/viewmodel/BaseViewModel.kt
+++ base/src/commonMain/kotlin/com/gdavidpb/tuindice/base/presentation/viewmodel/BaseViewModel.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onEach
@@ -23,40 +22,33 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalCoroutinesApi::class)
 abstract class BaseViewModel<S : ViewState, A : ViewAction, E : ViewEffect>(
-	name: String,
+	private val name: String,
 	initialState: S,
 	initialAction: A? = null,
 	dispatchers: TuIndiceDispatchers = DefaultTuIndiceDispatchers
 ) : ViewModel() {
-	private val effectChannel = Channel<E>(Channel.BUFFERED)
-	private val actionChannel = Channel<A>(Channel.BUFFERED)
+	private val effectChannel = Channel<E>(Channel.UNLIMITED)
+	private val actionChannel = Channel<A>(Channel.UNLIMITED)
 	private var lastPublishedState: String? = null
 
 	protected abstract val eventPublisher: EventPublisher
 
-	private val sendEffect: (E) -> Unit = { viewEffect ->
-		publishEffect(name, viewEffect)
-		if (!effectChannel.trySend(viewEffect).isSuccess)
-			viewModelScope.launch { effectChannel.send(viewEffect) }
-	}
-
-	val action = actionChannel.receiveAsFlow()
 	val effect = effectChannel.receiveAsFlow()
 
-	val state = action
+	val state = actionChannel.receiveAsFlow()
 		.onStart {
-			publishScreenView(name)
+			eventPublisher.publish(AppEvent.ScreenView(source = name))
 			if (initialAction != null) emit(initialAction)
 		}
-		.onEach { action -> publishAction(name, action) }
-		.flatMapMerge { action -> processAction(action, sendEffect) }
+		.onEach { action ->
+			eventPublisher.publish(AppEvent.Action(source = name, action = action.eventName()))
+		}
+		.flatMapMerge { action -> processAction(action, ::sendEffect) }
 		.scan(initialState) { currentState, mutation -> mutation(currentState) }
-		.distinctUntilChanged()
-		.onEach { state -> publishState(name, state) }
+		.onEach(::publishState)
 		.flowOn(dispatchers.default)
 		.stateIn(
 			scope = viewModelScope,
@@ -70,44 +62,30 @@ abstract class BaseViewModel<S : ViewState, A : ViewAction, E : ViewEffect>(
 	): Flow<Mutation<S>>
 
 	protected fun sendAction(viewAction: A) {
-		if (!actionChannel.trySend(viewAction).isSuccess)
-			viewModelScope.launch { actionChannel.send(viewAction) }
+		actionChannel.trySend(viewAction)
 	}
 
-	private fun publishScreenView(name: String) {
-		eventPublisher.publish(
-			AppEvent.ScreenView(source = name)
+	private fun sendEffect(viewEffect: E) {
+		val event = AppEvent.Effect(
+			source = name,
+			effect = viewEffect.eventName()
 		)
+
+		eventPublisher.publish(event)
+		effectChannel.trySend(viewEffect)
 	}
 
-	private fun publishAction(name: String, action: A) {
-		eventPublisher.publish(
-			AppEvent.Action(
-				source = name,
-				action = action.eventName()
-			)
-		)
-	}
-
-	private fun publishState(name: String, state: S) {
+	private fun publishState(state: S) {
 		val stateName = state.eventName()
 		if (lastPublishedState == stateName) return
 
 		lastPublishedState = stateName
-		eventPublisher.publish(
-			AppEvent.State(
-				source = name,
-				state = stateName
-			)
-		)
-	}
 
-	private fun publishEffect(name: String, viewEffect: E) {
-		eventPublisher.publish(
-			AppEvent.Effect(
-				source = name,
-				effect = viewEffect.eventName()
-			)
+		val event = AppEvent.State(
+			source = name,
+			state = stateName
 		)
+
+		eventPublisher.publish(event)
 	}
 }
